c1f7d3367deef08dbb780f83dadeb5822ce07222
Mariapalm98 <mariapalma.mpp@gmail.com>
2025-09-19 09:23:43 +0200

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,18 @@
-package se.iths.maria.helloworld;
+package se.iths.maria.projekt1;
 
-    public class HelloWorld {
-        public static void main(String[] args) {
-            System.out.println("Hello world");
-        }
-        
+public class HelloWorld {
+    public static void main(String[] args) {
+        //har har jag lagt in pratameter till min greeting i en arrey
+        String[] greeting = {"Hello", "World", "!"};
 
-
+        //anropar metod så greeting får string
+        myGreeting(greeting);
     }
+    private static void myGreeting(String[] greetings) {
+        for (String greeting : greetings) {
+
+            //här skriver jag ut pratameterna som är i greeting
+            System.out.println(greeting);
+        }
+    }
+}
